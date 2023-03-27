a613c9cc7c6810d9ab6c369e0f87252be86e0271
Anthony Romano <dentlabpa@gmail.com>
2023-03-27 15:13:18 -0400

    notes for 3/27

---
+++ js/main.js
@@ -0,0 +1,122 @@
+console.log('Hello from the main.js file!');
+
+// Basics of JavaScript
+// Comments using //
+
+/* 
+    Multi-line Comment in JavaScript 
+
+    --Variable Declaration in JavaScript
+    Primitive Data Types: String, Boolean, Number, Undefined, Null
+    Object Types: Object, Arrays, Functions
+*/
+
+
+// Variable Declaration in JavaScript
+// use 'var' to declare a globally-scoped or function-scoped variable
+// optional value - use camelCase to name variables
+
+// var myName;
+
+// console.log(myName);
+
+
+
+// String Declaration
+var myName = 'Anthony';
+
+// console.log() is equivalent to python's print() function
+console.log(myName);
+console.log(typeof myName);
+
+// Stings can be created using '' or "" or ``, The tick marks (``) have a special behavior to format the String
+// Tick Marks + ${} is the equivalent to f'{}' This is called and interpolated string
+console.log('My name is ${myName}');
+console.log("My name is ${myName}");
+console.log(`My name is ${myName}`);
+
+// NUMBER TYPES ALL FALL UNDER THE SAME TYPE OF 'number'
+// Integer
+var myAge = 99;
+console.log(myAge);
+console.log(typeof myAge);
+
+// Float
+var pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+// Boolean
+var trueBool = true;
+console.log(trueBool);
+console.log(typeof trueBool);
+
+var boolFalse = false;
+console.log(boolFalse);
+console.log(typeof boolFalse);
+
+// Undefined
+var something;
+console.log(something);
+console.log(typeof something);
+
+// Null
+var someNull = null;
+console.log(someNull);
+console.log(typeof someNull);
+
+// Object Data Type
+
+// Object
+var myObject = {};
+console.log(myObject);
+console.log(typeof myObject);
+
+var anotherObject = {
+    prop1: 'Hello World',
+    abc: 123
+};
+console.log(anotherObject);
+console.log(typeof anotherObject);
+
+// Array
+var firstArray = [];
+console.log(firstArray);
+console.log(typeof firstArray);
+
+var newArray = ['Anthony', 'Joe', 'James', 'Tyler']
+console.log(newArray)
+console.log(typeof newArray)
+
+// Function
+function abc(){};
+
+console.log(abc)
+console.log(typeof abc)
+
+
+// Accessing Properties from an object
+var person = {
+    first: 'Emily',
+    last: 'Murphy',
+    languages: ['JavaScript', 'Python']
+};
+
+console.log(person);
+
+// Bracket notation
+console.log(person['first'])
+// Dot notation
+console.log(person.last)
+
+console.log(person.languages)
+
+// When accessing properties in an array (aka the indices), use bracket notation
+console.log(person.languages[0])
+console.log(person.languages[1])
+
+// All array objects also have a 'length' property
+console.log(person.languages.length)
+
+//  Clear the console
+console.clear();
